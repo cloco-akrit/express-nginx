0fcf8a2a20cb8476553059cec1e9199afd57acdf
akrit <shrestha.akrit@cloco.co.jp>
2023-12-12 11:50:27 +0545

    server1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello to all");
+  res.send("hello to all\nfrom server 1");
 });
 
 app.listen("8000", () => {
